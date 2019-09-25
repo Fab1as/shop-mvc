d35b2d4f14cf3d853e313d8d5536df9066c1a366
Fab1as <fabiasser@gmail.com>
2019-09-25 23:21:57 +0300

    init

---
+++ ShopMvc/Models/Order.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopMvc.Models
+{
+    public class Order 
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime OrderPlacedTime { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string LastName { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string Address { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string City { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string Country { get; set; }
+
+        [StringLength(6)]
+        [Required]
+        public string ZipCode { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ ShopMvc/Models/OrderDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopMvc.Models
+{
+    public class OrderDetail 
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string LiquidName { get; set; }
+        public int Qty { get; set; }
+        public decimal Price { get; set; }
+
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
